b168b80d45beb4b3a7d1973ebc91507ba7ad8d47
Dung Thorfinn <ntdung89dn@gmail.com>
2017-10-13 09:05:35 +0700

    update with .gitignore

--- build/web/WEB-INF/classes/com/ttdk/gsonObject/DonWrapper.rs
+++
@@ -1,6 +0,0 @@
-com.ttdk.gsonObject.Sokhung
-com.ttdk.gsonObject.BenBaoDam
-com.ttdk.gsonObject.Bnbd
-com.ttdk.gsonObject.PhuongTien
-com.ttdk.gsonObject.BenNhanBaoDam
-com.ttdk.gsonObject.DonWrapper
